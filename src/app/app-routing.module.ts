85e2f16b30405b0f500de0586fd557cb1b993734
Rakshit Kumar <mail.me.at.rakshith@gmail.com>
2023-07-01 18:00:06 +0530

    modified:   src/app/app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,15 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterLoginComponent } from './components/register-login/register-login.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { AdminHomepageComponent } from './components/admin/admin-homepage/admin-homepage.component';
+import { AdminAddpageComponent } from './components/admin/admin-addpage/admin-addpage.component';
+import { AdminDetailpageComponent } from './components/admin/admin-detailpage/admin-detailpage.component';
+import { AdminUpdatepageComponent } from './components/admin/admin-updatepage/admin-updatepage.component';
+import { CustomerBookingpageComponent } from './components/cutomer/customer-bookingpage/customer-bookingpage.component';
+import { CustomerHomepageComponent } from './components/cutomer/customer-homepage/customer-homepage.component';
+import { CustomerProfilepageComponent } from './components/cutomer/customer-profilepage/customer-profilepage.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   {
@@ -72,24 +81,7 @@ const routes: Routes = [
   //   component:
   // }
 
-  {
-    path:"register",
-    component:RegisterComponent
-  },
-  {
-    path:"login",
-    component:LoginComponent
-  }
 
-  {
-    path: "customer/home",
-    component : CustomerHomepageComponent
-  },
-
-  {
-    path: "admin/home",
-    component: AdminHomepageComponent
-  },
   
 ];
 
