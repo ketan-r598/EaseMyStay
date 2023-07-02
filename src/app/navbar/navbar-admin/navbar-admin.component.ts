ee6753c0348c670bf278e75f1d36b108705aa21a
ketan_r <ketan_r598@outlook.com>
2023-07-02 17:46:06 +0530

    modified:   src/app/navbar/navbar-admin/navbar-admin.component.html 	modified:   src/app/navbar/navbar-admin/navbar-admin.component.ts

--- src/app/navbar/navbar-admin/navbar-admin.component.ts
+++ src/app/navbar/navbar-admin/navbar-admin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar-admin',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class NavbarAdminComponent {
 
+  constructor(
+    private router: Router
+  ) {}
+
+  logOut():void {
+    localStorage.clear();
+    this.router.navigate(['/login']);
+  }
 }
